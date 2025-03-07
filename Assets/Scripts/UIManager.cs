3552ed9726cb1ead99a62f7b9f555d2b54d0bda8
Dave-Raiev <161364283+Dave-Raiev@users.noreply.github.com>
2025-03-07 11:09:13 -0500

    Card UIManager
    Filling out field with card manager begun. Temp Card Bodies created for alpha

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public CardManager cardManager;
+    public GameObject[] cardSlots;
+
+
+    private void Start()
     {
-        
+        DisplayCards();
+    }
+    private void DisplayCards()
+    {
+        for (int i = 0; i < cardManager.cards.Count; i++)
+        {
+            cardSlots[i].transform.GetChild[0].GetComponent.<Image>.
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
