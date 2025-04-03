681a9159e485789fd15c7349c5facb8f2c53a39e
Ignacio Cajiao <163166083+IgnacioCajiao@users.noreply.github.com>
2025-04-03 02:19:34 -0400

    Add slider func & new items

--- Assets/Scripts/DialogueBattleManager.cs
+++ Assets/Scripts/DialogueBattleManager.cs
@@ -1,25 +1,32 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-using UnityEngine.EventSystems; 
+using UnityEngine.EventSystems;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class DialogueBattleManager : MonoBehaviour
 {
-    public TMP_Text promptText; 
-    public TMP_Text responseText; 
+    public TMP_Text promptText;
+    public TMP_Text responseText;
     public GameObject cardSelectionPanel;
-    public GameObject continueButton; 
+    public GameObject continueButton;
 
-    public SpriteRenderer enemySpriteRenderer; 
-    public Sprite neutralSprite; 
-    public Sprite correctResponseSprite; 
-    public Sprite incorrectResponseSprite; 
+    public SpriteRenderer enemySpriteRenderer;
+    public Sprite neutralSprite;
+    public Sprite correctResponseSprite;
+    public Sprite incorrectResponseSprite;
 
-    public GameObject hoverCardPanel; 
-    public Image hoverCardImage; 
+    public GameObject hoverCardPanel;
+    public Image hoverCardImage;
 
-    public List<DialoguePromptSO> prompts; 
+    public Slider infoSlider;
+    public Slider misinfoSlider;
+
+    private int correctSelections = 0;
+    private int incorrectSelections = 0;
+
+    public List<DialoguePromptSO> prompts;
     private int currentPromptIndex = 0;
 
     void Start()
@@ -43,7 +50,7 @@ public class DialogueBattleManager : MonoBehaviour
 
             foreach (Button card in cardSelectionPanel.GetComponentsInChildren<Button>())
             {
-                card.interactable = true; 
+                card.interactable = true;
             }
 
             continueButton.SetActive(false);
@@ -63,20 +70,32 @@ public class DialogueBattleManager : MonoBehaviour
         if (cardName == prompt.correctCardName)
         {
             responseText.text = prompt.correctCardSelected;
-            enemySpriteRenderer.sprite = correctResponseSprite; 
+            enemySpriteRenderer.sprite = correctResponseSprite;
+
+            correctSelections++;
+            infoSlider.value = (float)correctSelections / 9f;
 
             foreach (Button card in cardSelectionPanel.GetComponentsInChildren<Button>())
             {
-                if (card.name == cardName) 
+                if (card.name == cardName)
                 {
-                    card.gameObject.SetActive(false); 
+                    card.gameObject.SetActive(false);
                 }
             }
         }
         else
         {
             responseText.text = prompt.incorrectCardSelected;
-            enemySpriteRenderer.sprite = incorrectResponseSprite; 
+            enemySpriteRenderer.sprite = incorrectResponseSprite;
+
+            incorrectSelections++;
+            misinfoSlider.value = (float)incorrectSelections / 4f;
+
+            if (incorrectSelections >= 4)
+            {
+                ShowLoseMessage();
+                return;
+            }
         }
 
         responseText.gameObject.SetActive(true);
@@ -86,39 +105,54 @@ public class DialogueBattleManager : MonoBehaviour
             card.interactable = false;
         }
 
-        continueButton.SetActive(true); 
+        continueButton.SetActive(true);
     }
 
     public void OnContinueButtonClicked()
     {
-        currentPromptIndex++; 
-        ShowNextPrompt(); 
+        currentPromptIndex++;
+        ShowNextPrompt();
     }
 
     void EndBattle()
     {
-
         promptText.gameObject.SetActive(true);
         responseText.gameObject.SetActive(true);
 
         promptText.text = "You've successfully countered all arguments!";
-        responseText.text = ""; 
+        responseText.text = "";
 
         cardSelectionPanel.SetActive(false);
-
         continueButton.SetActive(false);
 
         enemySpriteRenderer.sprite = neutralSprite;
     }
 
+    void ShowLoseMessage()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = "Misinformation reached full! You lost!";
+
+        responseText.gameObject.SetActive(false);
+        cardSelectionPanel.SetActive(false);
+        continueButton.SetActive(false);
+
+        Invoke("RestartScene", 3f);
+    }
+
+    void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
     public void OnCardHover(Sprite largeCardSprite)
     {
-        hoverCardImage.sprite = largeCardSprite; 
-        hoverCardPanel.SetActive(true); 
+        hoverCardImage.sprite = largeCardSprite;
+        hoverCardPanel.SetActive(true);
     }
 
     public void OnCardExit()
     {
-        hoverCardPanel.SetActive(false); 
+        hoverCardPanel.SetActive(false);
     }
 }
